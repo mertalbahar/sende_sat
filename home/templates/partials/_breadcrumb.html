308d947f26b2bd74f7d279458b03af2ddbd74799
MertalBahar <mertalbahar@windowslive.com>
2023-11-28 18:20:57 +0300

    Product index view created. 1. Products listed in index view. 2. _product_card created and used in latest products and product index. 3. slider moved from base.html to home index. 4. Products link added in navbar.

---
+++ home/templates/partials/_breadcrumb.html
@@ -0,0 +1,4 @@
+<ul class="breadcrumb">
+    <li><a href="index.html">Home</a> <span class="divider">/</span></li>
+    <li class="active">Products Name</li>
+</ul>
--- product/views.py
+++ product/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Product
+
 
 def index(request):
-    return render(request, 'product/index.html')
+    products = Product.objects.all()
+    return render(request, 'product/index.html', {'products': products})
