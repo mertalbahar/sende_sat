ec10bb9b11176fda25fd8f1178b4cd71f888de61
MertalBahar <mertalbahar@windowslive.com>
2023-11-28 20:45:04 +0300

    Product detail page created. 1. Product detail page created, url redirections adjusted. 2. Product images added. 3. Related products added. 4. _product_list partial views created. 5. USE_THOUSAND_SEPARATOR added settings for price sepration.

--- product/views.py
+++ product/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import Product, ProductImages
 
 
 def index(request):
     products = Product.objects.all()
     return render(request, 'product/index.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug = slug)
+    images = ProductImages.objects.filter(product = product)
+    related_products = Product.objects.filter(category = product.category).exclude(slug = product.slug)
+    
+    context = {
+        'product': product,
+        'images': images,
+        'related_products': related_products
+    }
+    
+    return render(request, 'product/product_detail.html', context)
