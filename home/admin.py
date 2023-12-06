1853e9e7d2aa667a96eb60bcf132eaf24f6ee6da
MertalBahar <mertalbahar@windowslive.com>
2023-12-06 19:48:57 +0300

    ContactMessage model created. 1. ContactMessage model created. 2. ContactMessageForm created. 3. Page templates adjusted. 4. contact page created.

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import SiteSetting
+from .models import ContactMessage, SiteSetting
 
 
 @admin.register(SiteSetting)
 class SiteSettingAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
+
+
+@admin.register(ContactMessage)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'updated_at', 'status')
+    readonly_fields =('name', 'subject', 'email', 'message', 'ip')
+    list_filter = ['status']
--- home/views.py
+++ home/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+from .models import ContactMessage
+from .forms import ContactMessageForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactMessageForm(request.POST)
+        
+        if form.is_valid():
+            data = ContactMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız gönderilmiştir. Teşekkür ederiz.")
+            
+            return redirect('contact')
+        
+        else:
+            return render(request, 'contact.html', {'form': form})
+        
+    form = ContactMessageForm()
+    
+    return render(request, 'contact.html', {'form': form})
