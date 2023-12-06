7fd2948927a8c12d06435a458c49d76fe96fcad3
MertalBahar <mertalbahar@windowslive.com>
2023-12-06 15:19:10 +0300

    Products filtered by status is True. 1. Only product status = True will be shown. 2. users can see their products which product status = False 3. _product_card and _product_list updated. 4. product_detail updated. 5. related products in product_detail updated. 6. context_processors updated for latest and featured product

--- product/views.py
+++ product/views.py
@@ -5,7 +5,7 @@ from .models import Category, Product, ProductImages
 
 
 def index(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(status = True)
     paginator = Paginator(products, 6)
     page = request.GET.get('page', 1)
     page_obj = paginator.page(page)
@@ -23,7 +23,7 @@ def index(request):
 def product_detail(request, c_slug, p_slug):
     product = Product.objects.get(slug = p_slug)
     images = ProductImages.objects.filter(product = product)
-    related_products = Product.objects.filter(category = product.category).exclude(slug = product.slug)
+    related_products = Product.objects.filter(category = product.category).exclude(slug = product.slug).exclude(status = False)
     
     context = {
         'product': product,
@@ -36,8 +36,8 @@ def product_detail(request, c_slug, p_slug):
 
 def category_products(request, c_slug):
     category = Category.objects.get(slug = c_slug)
-    products = list(Product.objects.filter(category = category))
-    active_products = Product.objects.filter(category = category).count()
+    products = list(Product.objects.filter(category = category, status = True))
+    active_products = Product.objects.filter(category = category, status = True).count()
     
     node = category
     children = Category.objects.add_related_count(node.get_children(), Product, 'category', 'product_counts')
